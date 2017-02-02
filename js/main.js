5454e4e7861f58b21e9ce163e6d52720b110b80d
vnowen <viktoria.nowen@gmail.com>
2017-02-02 18:46:47 +0000

    Take back address bar on mobile

--- js/main.js
+++ js/main.js
@@ -4,9 +4,7 @@ $(document).ready(function() {
   var newSection = "";
 
   var vh = $(window).height();
-  $("html, body, .wrapper").css({
-    height: $(window).height()
-  });
+
   // make a function that fills the array automatic
   var sections = [[0, vh/2], [vh/2, vh + vh/2], [vh + vh/2, 2*vh + vh/2],[2*vh + vh/2, 3*vh + vh/2]]
   for (var i = 0; i < sections.length; i++) {
